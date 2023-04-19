57a7f0f9f2161db7ee0e9264dd4b1021cd10b4bd
Lucas <lucariel92@gmail.com>
2023-04-19 17:51:05 -0300

    update script

--- poligonize.R
+++ poligonize.R
@@ -2,19 +2,17 @@ library(raster)
 library(sf)
 library(rgdal)
 # plumber.R
-amba <- read_sf("C:/Users/HP/OneDrive/VizApp/app/data/amba.geojson")
-
-
+amba <- read_sf("C:/Users/HP/OneDrive/VizApp/git/vizapp/data/amba.geojson")
 
 #* Echo back the input
 #* @param p2r path to raster
 #* @get /p2r
-function(p2r="") {
-  nom = strsplit(p2r,'.tif')[[1]]
+function(p2r="", anio = 2021) {
+  nom = p2r
   
-  bp = "C:/Users/HP/OneDrive/VizApp/app/output_pred/"
-  p2r = paste0(bp,p2r)
-
+  bp = "C:/Users/HP/OneDrive/VizApp/apiPred/apiPred/output_pred/"
+  p2r = paste0(bp,p2r,"_",anio,".tif")
+  print(p2r)
   r1 = raster(p2r)
   
   r1[r1 < 10] = NA
@@ -22,8 +20,7 @@ function(p2r="") {
   ar <- aggregate(r1, fact=7)
   poligonized = rasterToPolygons(ar, dissolve = T)
   meuse_sf <- st_as_sf(poligonized)
-  
-  barrio = amba[amba$nombre_barrio==nom,]
+  barrio = amba[amba$renabap_id==nom,]
 
   meuse_sf = st_intersection(meuse_sf,barrio) 
   
